d572f54e9a1ab52d19d0107811a28963b3bbf814
Matheus Araujo <mun0t0@duck.com>
2025-05-31 22:05:47 -0300

    feat: added transaction fetchers

---
+++ frontend/src/fetchers/transactions/create.ts
@@ -0,0 +1,26 @@
+import { Transaction } from "@/types/transaction/types";
+import { handleResponse } from "../base";
+import { TransactionType } from "@/types/transaction/enum";
+
+const API_BASE_URL = import.meta.env.BASE_URL;
+
+export const createTransaction = async (
+    sourceAccountId: number,
+    destinationAccountId: number,
+    data: {
+      type: TransactionType;
+      amount: number;
+      description?: string;
+    }
+  ): Promise<Transaction> => {
+    const response = await fetch(
+      `${API_BASE_URL}/transactions/from/${sourceAccountId}/to/${destinationAccountId}`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await handleResponse<Transaction>(response);
+    return result.data!;
+}
---
+++ frontend/src/fetchers/transactions/get-all-of-user.ts
@@ -0,0 +1,10 @@
+import { Transaction } from "@/types/transaction/types";
+import { handleResponse } from "../base";
+
+const API_BASE_URL = import.meta.env.BASE_URL;
+
+export const getAllTransactionsOfUser = async (userId: number): Promise<Transaction[]> => {
+    const response = await fetch(`${API_BASE_URL}/transactions/user/${userId}`);
+    const result = await handleResponse<Transaction[]>(response);
+    return result.data!;
+  }
