c139ff35f6f09282518d54c9df47c98a7b7820b1
Matheus Araujo <mun0t0@duck.com>
2025-05-31 22:11:53 -0300

    feat: changed name variables

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/types/importMeta.d.ts" />
 
-interface ImportMetaEnv {
-  readonly KANEO_API_URL: string;
-}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
