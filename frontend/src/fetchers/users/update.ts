09048e3eda01707829aca1c42c9606279422ce36
Matheus Araujo <mun0t0@duck.com>
2025-05-31 22:08:04 -0300

    feat: added users fetchers to project

---
+++ frontend/src/fetchers/users/update.ts
@@ -0,0 +1,14 @@
+import { User } from "@/types/user/types";
+import { handleResponse } from "../base";
+
+const API_BASE_URL = import.meta.env.BASE_URL;
+
+export const updateUser = async (id: number, data: Partial<{ name: string; email: string }>): Promise<User> => {
+    const response = await fetch(`${API_BASE_URL}/users/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    const result = await handleResponse<User>(response);
+    return result.data!;
+}
